0069651c606528d0dc80993daa3622414bb640c4
Richard Hult <richard.hult@skf.com>
2022-12-07 16:14:59 +0100

    Initial Commit

---
+++ NoSuchTable/NoSuchTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSuchTableApp.swift
+//  NoSuchTable
+//
+//  Created by Richard Hult on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct NoSuchTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
